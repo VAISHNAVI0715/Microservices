1bff3a4f29cc480747b37cb8bf56d3920ede528c
vaishnavi <tusharvishi1515@gmail.com>
2025-02-16 16:20:04 +0530

    initial

---
+++ src/main/java/com/v/firstjobapp/job/JobController.java
@@ -0,0 +1,66 @@
+package com.v.firstjobapp.job;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job added successfully!", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted  = jobService.deleteJobByid(id);
+        if(deleted)
+            return new ResponseEntity<>("Job deleted successfully!", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //@PutMapping("/jobs/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                    @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id, updatedJob);
+        if(updated)
+            return new ResponseEntity<>("Job updated successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    /*
+    GET /jobs : Get all jobs
+    GET /jobs/{id} : Get a specific job by  id
+    POST /jobs: Create a new job with all job details
+    DELETE /jobs/{id} : Delete a specific job by id
+    PUT /jobs/{id}: Update a specific job
+    GET  /jobs/{id}/company : Get company associated with specific job id
+    * */
+}
---
+++ src/main/java/com/v/firstjobapp/job/JobService.java
@@ -0,0 +1,21 @@
+package com.v.firstjobapp.job;
+
+import java.util.List;
+
+public interface JobService {
+
+
+
+    List<Job> findAll();
+
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+
+    boolean deleteJobByid(Long id);
+
+    boolean updateJob(Long id, Job updatedJob);
+
+
+}
+
